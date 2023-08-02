5b0321b75d5d6b936c9c8b1c03ecf01c8197a351
syahrulutomo <irullvandalis@gmail.com>
2023-08-02 15:09:41 +0700

    feature: onChange location

--- src/components/Home/HomeFilter/Location/Location.tsx
+++ src/components/Home/HomeFilter/Location/Location.tsx
@@ -8,6 +8,7 @@ import { cities } from '@/model/mock-data/cities'
 
 const Location = () => {
   const [openLocation, setOpenLocation] = useState(false)
+  const [location, setLocation] = useState('')
 
   return (
     <Fragment>
@@ -15,14 +16,15 @@ const Location = () => {
         <div className='flex justify-between px-3'>
           <div className='flex'>
             <IconLocation className='fill-red-600 text-white' />
-            <p className='text-sm ml-3'>Your location</p>
+            <p className='text-sm ml-3'>{location ? location : 'Your Location'}</p>
           </div>
           <IconUnfold />
         </div>
       </div>
       <BottomSheet title="Location" open={openLocation} onClose={() => setOpenLocation(false)}>
         <div className='h-[280px] overflow-y-auto'>
-          {cities.map((item, index) => <div key={index} className='hover:bg-sky-200 rounded-sm px-2 py-3 border-b border-gray-400 last:border-none'>{item}</div>)}
+          {/* @ts-ignore  */}
+          {cities.map((item, index) => <div key={index} className='hover:bg-sky-200 rounded-sm px-2 py-3 border-b border-gray-400 last:border-none' onClick={(e) => { console.log(e.currentTarget.textContent); setLocation(e.currentTarget.textContent)}}>{item}</div>)}
         </div>
       </BottomSheet>
     </Fragment>
