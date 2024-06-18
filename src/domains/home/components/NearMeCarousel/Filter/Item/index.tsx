2a63c4967f78f5a6215926ce2f115825515e3358
syahrulutomo <irullvandalis@gmail.com>
2024-06-18 08:49:54 +0700

    fix: conditional rendering bottom content and fix carousel filter

--- src/domains/home/components/NearMeCarousel/Filter/Item/index.tsx
+++ src/domains/home/components/NearMeCarousel/Filter/Item/index.tsx
@@ -3,18 +3,19 @@ import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { twMerge } from "tailwind-merge"
 
 interface FilterItemProps {
+  itemKey: string;
   label: string;
   activeKey: string;
   onClick: () => void;
   Icon: IconDefinition;
 }
 
-const FilterItem = ({ label, activeKey, onClick, Icon }: FilterItemProps) => {
+const FilterItem = ({ itemKey, label, activeKey, onClick, Icon }: FilterItemProps) => {
   return (
     <div
       className={twMerge(
         'flex gap-2 items-center rounded-xl p-2 border-[1px] shadow-input',
-        activeKey === 'studio'
+        activeKey === itemKey
           ? 'text-primary-30 border-primary-30'
           : 'border-neutral-200 text-neutral-500',
       )}
--- src/domains/home/components/NearMeCarousel/Filter/index.tsx
+++ src/domains/home/components/NearMeCarousel/Filter/index.tsx
@@ -13,9 +13,9 @@ interface FilterProps {
 function Filter({ activeKey, setActiveKey }: FilterProps) {
   return (
     <div className="flex gap-2">
-      <FilterItem label='Session' Icon={faClock} onClick={() => setActiveKey('session')} activeKey={activeKey} />
-      <FilterItem label='Musician' Icon={faMusic} onClick={() => setActiveKey('musician')} activeKey={activeKey} />
-      <FilterItem label='Studio' Icon={faLocationDot} onClick={() => setActiveKey('studio')} activeKey={activeKey} />
+      <FilterItem itemKey='session' label='Session' Icon={faClock} onClick={() => setActiveKey('session')} activeKey={activeKey} />
+      <FilterItem itemKey='musician' label='Musician' Icon={faMusic} onClick={() => setActiveKey('musician')} activeKey={activeKey} />
+      <FilterItem itemKey='studio' label='Studio' Icon={faLocationDot} onClick={() => setActiveKey('studio')} activeKey={activeKey} />
     </div>
   )
 }
